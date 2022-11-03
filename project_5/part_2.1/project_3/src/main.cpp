f1503baaaf41b8b5e4fdb28bd89c6cd44a8b43dc
KrissiTekkari <dadasonk@gmail.com>
2022-11-03 15:05:40 +0000

    r

--- project_5/part_2.1/project_3/src/main.cpp
+++ project_5/part_2.1/project_3/src/main.cpp
@@ -33,16 +33,21 @@ Timer2_msec timer2;
 
 // char buffer[100];
 
-const size_t MSG_LEN = 6;
+const size_t MSG_LEN = 8;
 uint8_t msg[MSG_LEN];
 
 int reg[3] = {0, 0, 0}; // {commands, speed, reference speed}
+const size_t REG_LEN = sizeof(reg) /sizeof(reg[0]);
 
 uint8_t server_address;
 uint8_t function_code;
 uint16_t register_address;
 uint16_t register_value;
 uint16_t total_to_read;
+
+uint16_t crc_received; 
+uint16_t crc_calculated;
+uint16_t crc_send;
 
 int &command = reg[0];
 
@@ -64,11 +69,28 @@ float Ti = 0.048;
 uint8_t led_freq = 1;
 bool cont = false;
 
-// for storing speed and duty cycle values for the plots!!!
-// float speed_array[200];
-// double duty_cycle_array[200];
-// int index = 0;
-// bool start = false;
+uint16_t ModRTU_CRC(uint8_t buf[], int len)
+{
+    uint16_t crc = 0xFFFF;
+
+    for (int pos = 0; pos < len; pos++)
+    {               
+        crc ^= (uint16_t)buf[pos];          // XOR byte into least sig. byte of crc
+
+        for (int i = 8; i != 0; i--)
+        {
+            if ((crc & 0x0001) != 0)
+            {
+                crc >>= 1;
+                crc ^= 0xA001;
+            }
+            else
+                crc >>= 1;
+        }
+    }
+    return crc;
+}
+
 
 Context *context;
 
@@ -94,6 +116,15 @@ void loop()
 	if (Serial.available() > 0) // bytes received
 	{
 		Serial.readBytes(msg, MSG_LEN); // binary messages have fixed length and no terminating \0.
+		// check the CRC
+		// if CRC does not match, discard the message
+		crc_received = (msg[MSG_LEN - 2] << 8) | msg[MSG_LEN - 1];
+		crc_calculated = ModRTU_CRC(msg, MSG_LEN - 2);
+		// crc_received does not match crc_calculated send no response
+		if (crc_received != crc_calculated)
+		{
+		return;
+		}
 		server_address = msg[0];
 		function_code = msg[1];
 
@@ -104,6 +135,19 @@ void loop()
 			case 6: // WRITE
 				// turn two bytes into a 16-bit integer
 				register_address = (msg[2] << 8) | msg[3];
+				// if illegal register address, send exception response
+				if (register_address > REG_LEN)
+				{
+					buffer[0] = server_address;
+					buffer[1] = function_code + 0x80;
+					buffer[2] = 0x02;
+					crc_send = ModRTU_CRC(buffer, 3);
+					buffer[3] = (crc_send >> 8) & 0xFF;
+					buffer[4] = crc_send & 0xFF;
+					Serial.write(buffer, 5);
+					return;
+				}
+				
 				register_value = (msg[4] << 8) | msg[5];
 				reg[register_address] = register_value;
 
@@ -131,7 +175,18 @@ void loop()
 					last_command = command;
 					}
 
-				memcpy(buffer, msg, MSG_LEN);
+				buffer[0] = server_address;
+				buffer[1] = function_code;
+				buffer[2] = msg[2];
+				buffer[3] = msg[3];
+				buffer[4] = msg[4];
+				buffer[5] = msg[5];
+
+				// calculate the CRC
+				crc_send = ModRTU_CRC(buffer, MSG_LEN-2);
+				buffer[MSG_LEN-2] = (crc_send >> 8) & 0xFF;
+				buffer[MSG_LEN-1] = crc_send & 0xFF;
+
 				Serial.write(buffer, MSG_LEN);
 				break;
 			case 3: // READ
@@ -140,6 +195,20 @@ void loop()
 				//speed_reg = speed + 140;
 				register_address = (msg[2] << 8) | msg[3];
 				total_to_read = (msg[4] << 8) | msg[5];
+
+				// if illegal register address, send exception response
+				if (register_address + total_to_read > REG_LEN)
+				{
+					buffer[0] = server_address;
+					buffer[1] = function_code + 0x80;
+					buffer[2] = 0x02;
+					crc_send = ModRTU_CRC(buffer, 3);
+					buffer[3] = (crc_send >> 8) & 0xFF;
+					buffer[4] = crc_send & 0xFF;
+					Serial.write(buffer, 5);
+					return;
+				}
+
 				buffer[0] = server_address;
 				buffer[1] = function_code;
 				buffer[2] = total_to_read * 2;
@@ -150,9 +219,23 @@ void loop()
 					buffer[3 + i * 2] = (reg[register_address + i] >> 8) & 0xFF;
 					buffer[4 + i * 2] = reg[register_address + i] & 0xFF;
 				}
+				// calculate the CRC
+				crc_send = ModRTU_CRC(buffer, 3 + total_to_read * 2);
+				buffer[3 + total_to_read * 2] = (crc_send >> 8) & 0xFF;
+				buffer[4 + total_to_read * 2] = crc_send & 0xFF;
 				// send the buffer back to the client
 				Serial.write(buffer, 3 + total_to_read * 2);
 			default:
+			          // illegal function code
+				buffer[0] = server_address;
+				buffer[1] = function_code | 0x80;
+				buffer[2] = 0x01; //exception code 0x01
+				// calculate the CRC
+				crc_send = ModRTU_CRC(buffer, 3);
+				buffer[3] = (crc_send >> 8) & 0xFF;
+				buffer[4] = crc_send & 0xFF;
+				// send the buffer back to the client
+				Serial.write(buffer, 5);
 				break;
 			}
 		}
